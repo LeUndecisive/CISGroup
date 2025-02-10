e9f079902fab4dff6a8e8e9f44186880f9fddcf7
nicholashuish <nicholas.huish@gmail.com>
2025-02-10 15:30:19 -0700

    Final Update for Sorting.

--- bubbleSort.c
+++ bubbleSort.c
@@ -7,14 +7,12 @@
 
 void bubbleSort(int sort_Array[], int n) { // Function sorts the array of random integers. 
 	
-int num_numbers = 25;	
-int Array[num_numbers]; 
 int i; // To iterate through the array.
 int j; // To keep track of what is being swapped. 
 int temp; // Acts as a place holder for swapping variables. 
 
-     for (i = 0; i < n - 1;  i++){
-          for (j = 0; j < n - i - 1; j++){
+     for (i = 0; i < n - 1;  i++){ 
+          for (j = 0; j < n - i - 1; j++){ 
                if (sort_Array[j] > sort_Array[j + 1]){ // The whole entire loop utilizes a bubble sort algorithm. 
                    temp = sort_Array[j]; 
 				   sort_Array[j] = sort_Array[j + 1];
@@ -24,45 +22,20 @@ int temp; // Acts as a place holder for swapping variables.
 	 }
 } 
 
-void printArray( int  sort_Array[], int size) {
+void printArray( int sort_Array[], int size) { // Function prints the unsorted and sorted arrays. 
 
-	int i; // Iterate through array of integers. 
-
-	for (i = 0; i < size; ++i) {
-		
-		// Going to output 25 random sorted integers between 1 and 1000.
-		printf("%d ", sort_Array[i]);
-	}
-}
-
-int main() {
-	
-	/*The program is going to generate a list of numbers
-	   and then sort them in ascending order. */
-	
-	int currentArray;
-	int num_numbers = 25; // Program can generate 25 total numbers. 
-	int min = 1;
-	int max = 1000;
 	int i;
-	int Array[num_numbers]; 
-	int size = sizeof(Array) / sizeof(Array[0]); // Need to indicate the real size of the array. 
-	
-	srand(time(0)); // Will generate a new set of numbers every time the program is run. The "0" is required.
-	
-	currentArray = (rand() % 1000) + 1; // Will generate a number between 1 - 1000.
-	
-	printf("Unsorted Array: \n");
 	
 	// Will output multiple unsorted numbers between 1 - 1000. 
-	for (i = 0; i < num_numbers; ++i) {
-		Array[i] = rand() % (max - min + 1) + min; // Need an end value since we start from index 0. 
-		printf("%d ", Array[i]);
+	printf("Unsorted Array: \n");
+	
+	for (i = 0; i < size; ++i) {
+		printf("%d ", sort_Array[i]);
 	}
 	
 	printf("\n");
 	
-	bubbleSort(Array, size); // Calling in a sorting algorithm. 
+	bubbleSort(sort_Array, size); // Calling a function to sort the unsorted array. 
 	
 	printf("\n");
 	
@@ -70,17 +43,36 @@ int main() {
 	
 	printf("\n");
 	
-	printArray(Array, size);
-
-	return 0;
+	// Going to output 25 random sorted integers between 1 and 1000.
+	for (i = 0; i < size; ++i) {
+		printf("%d ", sort_Array[i]);
+	}
+	
+	printf("\n");
 }
 
-// Steps to implement bubble sort:
+int main() {
+	
+	/*The program is going to generate a list of numbers
+	   and then sort them in ascending order. */
+	
+	int Array[25];
+	int currentArray;
+	int i;
+	int max = 1000;
+	int min = 1;
+	
+	srand(time(0)); // Will generate a new set of numbers every time the program is run. The "0" is required.
+	
+	currentArray = (rand() % 1000) + 1; // Will generate a number between 1 - 1000.
 
-    /* In first cycle,
-        Start by comparing 1st and 2nd element and swap if 1st element is greater.
-        After that do the same for 2nd and 3rd element.
-        At the end of cycle you will get max element at the end of list.
-    Now do the same in all subsequent cycles.
-    Perform this for (number of elements – 1) times.
-    You will get sorted list. */
+	// Will now generate an array of 25 random integers ranging between 1 - 1000.
+	for (i = 0; i < 25; ++i) {
+		Array[i] = rand() % (max - min + 1) + min; // Need an end value since we start from index 0. 
+	}
+	
+	// Calling a function to output a sorted and an unsorted array.
+	printArray(Array, 25); 
+	 
+	return 0;
+}
