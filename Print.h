7c8c263c2aa04e6111dacb4c90fc90b0edfc9ff2
Atha3-S <merkinsw383@students.cochise.edu>
2025-04-26 19:20:35 -0400

    Create Print.h

---
+++ Print.h
@@ -0,0 +1,9 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include "Main.h"  // Include the main header for structures
+
+// Function declaration for printing all data in a list
+void PrintALL(List* SentList);
+
+#endif // PRINT_H
