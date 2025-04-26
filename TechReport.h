0eb6b6958330102cc3765713d335938877ce24c0
Atha3-S <merkinsw383@students.cochise.edu>
2025-04-26 19:19:47 -0400

    Create TechReport.h

---
+++ TechReport.h
@@ -0,0 +1,9 @@
+#ifndef TECHREPORT_H
+#define TECHREPORT_H
+
+#include "Main.h"  // Include the main header for structures
+
+// Function declarations for reporting
+void print_to_file(FILE *inFile, const char *data, int numbers);
+
+#endif // TECHREPORT_H
