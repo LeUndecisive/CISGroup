25bd1be81692d961e71e5965eef9548cfc8bdde5
Atha3-S <merkinsw383@students.cochise.edu>
2025-04-26 19:20:09 -0400

    Create Menu.h

---
+++ Menu.h
@@ -0,0 +1,9 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "Main.h"  // Include the main header for structures
+
+// Function declaration for menu handling
+void menu(DataContext *ctx);
+
+#endif // MENU_H
