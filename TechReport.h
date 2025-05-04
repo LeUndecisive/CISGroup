739a72dd4f42648e8d5969aa4177ea127e704e5e
LeUndecisive <joshs.doorway@gmail.com>
2025-05-03 21:50:05 -0700

    Update TechReport.h

--- TechReport.h
+++ TechReport.h
@@ -1,10 +1,53 @@
 #ifndef TECHREPORT_H
 #define TECHREPORT_H
-
-#include "TechReport.h"
+#include "Loader.h"
 #include "Main.h"  // Include the main header for structures
 
 // Function declarations for reporting
-void print_to_file(FILE *inFile, const char *data, int numbers);
+
+/**-----Prints-All-IDS-In-List------------
+    Used when the user provided ID fails
+*/
+void PrintALL_IDS(List* SentList);
+
+/**---Prints-Data-From-OS-Node----------
+    Used when the user provided ID fails
+*/
+void PrintNode_OS(Node* SentNode);
+
+/**------------------------------------
+
+*/
+void PrintNode_RS();
+
+/**------------------------------------
+
+*/
+void PrintNode_HV();
+
+/**------------------------------------
+
+*/
+void PrintNode_PS();
+
+/**------------------------------------
+
+*/
+void PrintALL_OS(List* SentList);
+
+/**------------------------------------
+
+*/
+void PrintALL_RS(List* SentList);
+
+/**------------------------------------
+
+*/
+void PrintALL_HV(List* SentList);
+
+/**------------------------------------
+
+*/
+void PrintALL_PS(List* SentList);
 
 #endif // TECHREPORT_H
