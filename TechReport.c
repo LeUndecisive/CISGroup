7b4b7950e30203a5193c8153e5004b82affda73b
Atha3-S <merkinsw383@students.cochise.edu>
2025-05-07 22:32:01 -0400

    Updated TechReport, Added Manual
    Completed Info dump in TechReport for Hypervisor List , Product List, and Hypervisor Support. Also Created the Manual PDF with instructions for use of the exe, what the expected outputs will be and what expected inputs are

--- TechReport.c
+++ TechReport.c
@@ -48,10 +48,18 @@ void PrintALL_OS(List* SentList){
 /**----Prints-The-Data-Of-RS-Node--------------------------
 
 */
-void PrintNode_RS(){
-
-    printf("\n\n [PrintNode_RS-TechReport.c] \n\n");
-
+void PrintNode_RS(Node* SentNode){
+    //printf("\n\n [PrintNode_RS-TechReport.c] \n\n");
+    if (SentNode == NULL) {
+        printf("Error: RS Node is NULL.\n");
+        return;
+    }
+    // Printing details of the RS Node
+    printf("ID: %s || ", SentNode->ID);
+    printf("Name: %s || ", SentNode->Name);
+    printf("Version: %s || ", SentNode->Version);
+    printf("Platform: %s || ", SentNode->Platform);
+    printf("Release Date: %s\n", SentNode->ReDate);
 };
 
 
@@ -59,7 +67,22 @@ void PrintNode_RS(){
 
 */
 void PrintALL_RS(List* SentList){
-    printf("\n\n [PrintALL_RS-TechReport.c] \n\n");
+    //printf("\n\n [PrintALL_RS-TechReport.c] \n\n");
+    if (SentList == NULL || SentList->Head == NULL) {
+        printf("Error: List is empty or NULL.\n");
+        return;
+    }
+
+    printf("\n--- RS Node Report ---\n");
+    Node* CurrentNode = SentList->Head;
+
+    while (CurrentNode != NULL) {
+        PrintNode_RS(CurrentNode);  // Call PrintNode_RS to print each node's data
+        printf("\n");  // Print a newline between nodes
+        CurrentNode = CurrentNode->next;  // Move to the next node in the list
+    }
+
+    printf("\n--- End of RS Node Report ---\n");
 };
 
 
@@ -67,10 +90,13 @@ void PrintALL_RS(List* SentList){
 /**----Prints-The-Data-Of-HV-Node--------------------------
 
 */
-void PrintNode_HV(){
+void PrintNode_HV(Node* SentNode){
 
-    printf("\n\n [PrintNode_HV-TechReport.c] \n\n");
-
+    //printf("\n\n [PrintNode_HV-TechReport.c] \n\n");
+    printf("ID: %s || ", SentNode->ID);
+    printf("Name: %s || ", SentNode->Name);
+    printf("Version: %s || ", SentNode->Version);
+    printf("Release Date: %s", SentNode->ReDate);
 };
 
 
@@ -79,14 +105,35 @@ void PrintNode_HV(){
 
 */
 void PrintALL_HV(List* SentList){
-    printf("\n\n [Temp] \n\n");
+    //printf("\n\n [Temp] \n\n");
+    printf("\n--- Hypervisor Report ---\n");
+    Node* CurrentNode = SentList->Head;
+    if (!CurrentNode) {
+        printf("No hypervisors found in the system.\n");
+        return;
+    }
+
+    while (CurrentNode != NULL) {
+        PrintNode_HV(CurrentNode);
+        printf("\n");
+        CurrentNode = CurrentNode->next;
+    }
+    printf("\n--- End of Report ---\n");
 };
 
 /**----Prints-The-Data-Of-PS-Node--------------------------
-    This one may not be needed as this is not an option yet
+ * 
 */
-void PrintNode_PS(){
-    printf("\n\n [PrintNode_PS-TechReport.c] \n\n");
+void PrintNode_PS(Node* SentNode){
+    //printf("\n\n [PrintNode_PS-TechReport.c] \n\n");
+    printf("ID: %s || ", SentNode->ID);
+    printf("Version: %s || ", SentNode->Version);
+    printf("Hardware: %s || ", SentNode->Hardware);
+    printf("Release Date: %s\n", SentNode->ReDate);
+
+    printf("  Supported OS: %s\n", SentNode->Supported_OS[0] ? SentNode->Supported_OS : "None");
+    printf("  Supported RS: %s\n", SentNode->Supported_RS[0] ? SentNode->Supported_RS : "None");
+    printf("  Supported HS: %s\n", SentNode->Supported_HV[0] ? SentNode->Supported_HV : "None");
 };
 
 
@@ -97,5 +144,20 @@ void PrintNode_PS(){
 
 */
 void PrintALL_PS(List* SentList){
-    printf("\n\n [PrintALL_PS-TechReport.c] \n\n");
+    //printf("\n\n [PrintALL_PS-TechReport.c] \n\n");
+    printf("\n--- Product Report ---\n");
+    Node* CurrentNode = SentList->Head;
+
+    if (!CurrentNode) {
+        printf("No products found in the system.\n");
+        return;
+    }
+
+    while (CurrentNode != NULL) {
+        PrintNode_PS(CurrentNode);
+        printf("\n");
+        CurrentNode = CurrentNode->next;
+    }
+
+    printf("--- End of Product Report ---\n");
 };
